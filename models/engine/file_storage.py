2560e40a52732b6957cadf44a0cf49ae6763a405
sassihamdi-CD <hamdysassy7@gmail.com>
2023-09-20 23:28:04 +0100

    Done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
@@ -8,7 +15,7 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-def all(self, cls=None):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         if cls:
             if type(cls) == str:
@@ -35,13 +42,6 @@ def all(self, cls=None):
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
 
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
@@ -53,12 +53,18 @@ def all(self, cls=None):
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
-def delete(self, obj=None):
+    def delete(self, obj=None):
         """Deletes and object from __objects"""
         if (obj):
             key = "{}.{}".format(type(obj).__name__, obj.id)
             del self.__objects[key]
+
+    def close(self):
+        """Calls reload method for deserializing
+        the JSON file to objects
+        """
+        self.reload()
