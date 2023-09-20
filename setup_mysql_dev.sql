d947d0deb1e421214aa934405d75f48677a3ec4a
sassihamdi-CD <hamdysassy7@gmail.com>
2023-09-20 17:59:51 +0100

    modife database setup

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,6 @@
--- Create a new user and new database
+-- Creates a new user and a new database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev@localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 FLUSH PRIVILEGES;
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev@localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
